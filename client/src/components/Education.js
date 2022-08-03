f720ae9b00f25b4f64d7cbe589af339f3dfd3e85
kennethachan <kennethandrechan@gmail.com>
2022-08-03 12:31:32 -0400

    initial education layout added

--- client/src/components/Education.js
+++ client/src/components/Education.js
@@ -1,17 +1,25 @@
 import React from 'react';
+import "./Education.css"
+import education from "../pictures/school.png"
 
 function Education(props) {
     return (
-        <div className="education">
+        <div className="education-container">
+          <div>
+            <img className="education-logo" src={education}></img>
+            </div>
+        <div className="education-wrapper">
         <h2>Education</h2>
-        <p>
-          General Assembly: Software Engineering Immersive (June 2022 - Sept
+        <li className="list">
+          <span className="school">General Assembly</span>: Software Engineering Immersive <br/>
+          (June 2022 - Sept
           2022)
-        </p>
-        <p>
+        </li>
+        {/* <li>
           HTML, CSS, Javascript, React, MongoDB, React Redux, PostgreSQL,
           Sequelize, Vue.js, Python, Django
-        </p>
+        </li> */}
+        </div>
       </div>
     );
 }
