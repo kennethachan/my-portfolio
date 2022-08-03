aa804f1f43087edefb1a214894cd7d8f789d7c84
kennethachan <kennethandrechan@gmail.com>
2022-08-02 23:09:28 -0400

    initial about me layout added

--- client/src/components/AboutMe.js
+++ client/src/components/AboutMe.js
@@ -3,20 +3,22 @@ import "./AboutMe.css"
 
 function AboutMe(props) {
     return (
-        <div className="aboutme">
+        <div className="aboutme-container">
+          <div>
         <img
           className="profile-pic"
           src="https://media-exp2.licdn.com/dms/image/C4E03AQGA9Gmt-jTxYA/profile-displayphoto-shrink_200_200/0/1657225082823?e=1663200000&v=beta&t=EzZYtu3lA8YIhn8r7SYYaAztnWEUqIgyJHAYSckL_yk"
         ></img>
-         <h1>Kenneth Chan</h1>
-        <h2>about me</h2>
+         </div>
+         <div className="aboutme">
         <p className="brand">
-          I am an Enthusiastic full stack developer based in the NYC area with a
+          Hello! my name is <span className="name">Kenneth Chan</span>. I am an Enthusiastic full stack developer based in the NYC area with a
           diverse background ranging from software engineering to
           architectural design. I am a creator. Always curious. I have a
           strong ambition to create work that is uniquely enjoyable and
           fulfilling to the user’s experience.
         </p>
+        </div>
       </div>
     );
 }
