52a307d7af4ffe94694aec74e8c5215f8a57ff0b
kennethachan <kennethandrechan@gmail.com>
2022-08-03 18:01:06 -0400

    updated resume layout

--- client/src/pages/Resume.js
+++ client/src/pages/Resume.js
@@ -5,7 +5,7 @@ import { Link } from "react-router-dom"
 function Resume(props) {
     return (
         <div>
-        <header className="header">
+        <header className="res-header">
         <div className="links">
         <Link className="resume" to="/">Back to Home</Link>
         <a className="github" href="https://github.com/kennethachan" target="_blank">
@@ -15,7 +15,7 @@ function Resume(props) {
         LinkedIn
         </a>
         </div>
-      </header>
+      </header>>
         <div className="resume-container">
            <h1>Kenneth Chan</h1>
 <li className="res-list">Full Stack Software Engineer</li>
@@ -30,32 +30,32 @@ function Resume(props) {
 <h3>RELEVANT EXPERIENCE</h3>
 <h4>Software Engineering Immersive Fellow | General Assembly | Remote </h4>
 <h5 className="res-list">June 2022 - September 2022</h5>
-<li className="res-list">Completed 500+ hours of expert-led instruction in JavaScript, Python, MongoDB, Node, React, and hands-on learning of web and mobile application fundamentals using the industry's most in-demand technologies. Developed projects, including:</li>
-<li className="res-list"><span className="bold-text">Lucidity:</span> Lucidity is a “lofi anime” inspired productivity site to promote a positive environment for studying/relaxing built using MERN.  The site features registration/login authorization.  Once at the home page the notepad possesses full CRUD, a GIF slideshow that lets the user add/delete GIFs, and audio selection.</li>
-<li className="res-list"><span className="bold-text">Kanto Region Pokedex:</span>Built with vanilla Javascript, HTML and CSS.  Based on the game series/show, the pokedex allows the user to randomly generate a pokemon card with stats and an image, user can type in a pokemon name to generate a specified pokemon card. Pokedex also features a dark mode option.</li>
-<li className="res-list"><span className="bold-text">Tic Tac Toe:</span> Built with vanilla Javascript, HTML and CSS.  A classic 2 player game with a gloomy night drive theme</li>
+<li className="res-list"><span className="bold-text">-</span>Completed 500+ hours of expert-led instruction in JavaScript, Python, MongoDB, Node, React, and hands-on learning of web and mobile application fundamentals using the industry's most in-demand technologies. Developed projects, including:</li>
+<li className="res-list"><span className="bold-text">-Lucidity:</span> Lucidity is a “lofi anime” inspired productivity site to promote a positive environment for studying/relaxing built using MERN.  The site features registration/login authorization.  Once at the home page the notepad possesses full CRUD, a GIF slideshow that lets the user add/delete GIFs, and audio selection.</li>
+<li className="res-list"><span className="bold-text">-Kanto Region Pokedex:</span>Built with vanilla Javascript, HTML and CSS.  Based on the game series/show, the pokedex allows the user to randomly generate a pokemon card with stats and an image, user can type in a pokemon name to generate a specified pokemon card. Pokedex also features a dark mode option.</li>
+<li className="res-list"><span className="bold-text">-Tic Tac Toe:</span> Built with vanilla Javascript, HTML and CSS.  A classic 2 player game with a gloomy night drive theme</li>
 
 
 <h4>Project Assistant (Estimator/Assistant Project Manager)| Ferra Designs | Brooklyn,NY</h4>
 <h5>September 2021 - June 2022</h5>
-<li className="res-list">Performed take off/ analyzed drawings & specifications for developers, collaborated with general contractors, institutions, and commercial establishments to determine scope of work and budget for custom metal fabrication up to $12M</li>
-<li className="res-list">Prepared detailed takeoffs, counts, scope summaries, research material/components for many projects ex. “Waldorf Astoria”</li>
-<li className="res-list">Coordinated  and assisted post sale transition with  PMs and Design Manager.</li>
+<li className="res-list"><span className="bold-text">-</span>Performed take off/ analyzed drawings & specifications for developers, collaborated with general contractors, institutions, and commercial establishments to determine scope of work and budget for custom metal fabrication up to $12M</li>
+<li className="res-list"><span className="bold-text">-</span>Prepared detailed takeoffs, counts, scope summaries, research material/components for many projects ex. “Waldorf Astoria”</li>
+<li className="res-list"><span className="bold-text">-</span>Coordinated  and assisted post sale transition with  PMs and Design Manager.</li>
 
 <h4>Career Break | Health & Well-Being</h4>
 <h5>July 2020 - September 2021</h5>
-<li className="res-list">Voluntary leave due to health/family concerns during Covid 19 pandemic.</li>
+<li className="res-list"><span className="bold-text">-</span>Voluntary leave due to health/family concerns during Covid 19 pandemic.</li>
 
 <h4>Lead Estimator | Crown Signs | Mt. Vernon,NY</h4>
 <h5>May 2016 - July 2020</h5>
-<li className="res-list">Promoted from Assistant Estimator to Lead Estimator based on exemplary performance to provide fully developed budgets.</li>
-<li className="res-list">Performed take off/ analyzed drawings & specifications for developers, general contractors, institutions, and commercial establishments to determine scope of work and budget for signage up to $500,000(+).</li>
-<li className="res-list">Developed and maintained a bid tracker to monitor open, won, lost bids & project follow ups on google sheets.</li>
-<li className="res-list">Assisted in training (3) estimators as well as (3) project managers.</li>
+<li className="res-list"><span className="bold-text">-</span>Promoted from Assistant Estimator to Lead Estimator based on exemplary performance to provide fully developed budgets.</li>
+<li className="res-list"><span className="bold-text">-</span>Performed take off/ analyzed drawings & specifications for developers, general contractors, institutions, and commercial establishments to determine scope of work and budget for signage up to $500,000(+).</li>
+<li className="res-list"><span className="bold-text">-</span>Developed and maintained a bid tracker to monitor open, won, lost bids & project follow ups on google sheets.</li>
+<li className="res-list"><span className="bold-text">-</span>Assisted in training (3) estimators as well as (3) project managers.</li>
 
 <h3>EDUCATION</h3>
-<li className="res-list">General Assembly | Software Engineering Immersive | Remote | June 2022 - September 2022</li>
-<li className="res-list">Architectural Engineering | University of Hartford | Hartford,CT | September 2012 - May 2016 | 3.4 GPA</li>
+<li className="res-list"><span className="bold-text">-</span>General Assembly | Software Engineering Immersive | Remote | June 2022 - September 2022</li>
+<li className="res-list"><span className="bold-text">-</span>Architectural Engineering | University of Hartford | Hartford,CT | September 2012 - May 2016 | 3.4 GPA</li>
 
         </div>
         </div>
