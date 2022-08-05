295a39631d8f372f658b3a8cc02c67de64d0b245
kennethachan <kennethandrechan@gmail.com>
2022-08-04 21:45:28 -0400

    added hover effects to buttons

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,22 +1,34 @@
-import React from 'react';
+import React from "react"
 import Resume from "../pages/Resume"
 import { Link } from "react-router-dom"
 import "./Header.css"
 
 function Header(props) {
-    return (
-        <header className="header">
+  return (
+    <div>
+      <header className="header">
         <div className="links">
-        <Link className="resume" to="/resume">Resume</Link>
-        <a className="github" href="https://github.com/kennethachan" target="_blank">
-          Github
-        </a>
-        <a className="linkedin" href="https://www.linkedin.com/in/kennethachan/" target="_blank">
-        LinkedIn
-        </a>
+          <Link className="resume" to="/resume">
+            Resume
+          </Link>
+          <a
+            className="github"
+            href="https://github.com/kennethachan"
+            target="_blank"
+          >
+            Github
+          </a>
+          <a
+            className="linkedin"
+            href="https://www.linkedin.com/in/kennethachan/"
+            target="_blank"
+          >
+            LinkedIn
+          </a>
         </div>
       </header>
-    );
+    </div>
+  )
 }
 
-export default Header;
+export default Header
