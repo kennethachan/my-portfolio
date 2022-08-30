2094741b73dec8c2d669bb522d72e33e5e873214
kennethachan <kennethandrechan@gmail.com>
2022-08-30 14:20:56 -0400

    all skill icons integrated

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -10,6 +10,15 @@ import react from "../pictures/react.png"
 import vue from "../pictures/vue.png"
 import python from "../pictures/python.png"
 import dj from "../pictures/django.png"
+import node from "../pictures/node.png"
+import ex from "../pictures/Express.png"
+import jquery from "../pictures/jquery.svg"
+import mongoose from "../pictures/mongoose.svg"
+import mongodb from "../pictures/mongodb.png"
+import postgresql from "../pictures/postgresql.png"
+import git from "../pictures/git.png"
+import github from "../pictures/github.png"
+import api from "../pictures/api.png"
 
 function Skills(props) {
   return (
@@ -20,22 +29,28 @@ function Skills(props) {
       <div>
         <Tabs>
           <TabPane name="Programming Languages" key="1">
-            <img className="html" src={html}></img>HTML |
-            <img className="css" src={css}></img>CSS |{" "}
-            <img className="js" src={js}></img>JavaScript |{" "}
-            <img className="react" src={react}></img>React.js |{" "}
-            <img className="vue" src={vue}></img>Vue.js |{" "}
-            <img className="python" src={python}></img> Python3 |{" "}
-            <img className="dj" src={dj}></img> Django
+            <img className="skill-icon" src={html}></img>HTML |
+            <img className="skill-icon" src={css}></img>CSS |{" "}
+            <img className="skill-icon" src={js}></img>JavaScript |{" "}
+            <img className="skill-icon" src={react}></img>React.js |{" "}
+            <img className="skill-icon" src={vue}></img>Vue.js |{" "}
+            <img className="skill-icon" src={python}></img> Python3 |{" "}
+            <img className="skill-icon" src={dj}></img> Django
           </TabPane>
           <TabPane name="Frameworks & Libraries" key="2">
-            Node.js | Express | jQuery | Mongoose
+            <img className="skill-icon" src={node}></img>Node.js |{" "}
+            <img className="skill-icon" src={ex}></img>Express |{" "}
+            <img className="skill-icon-jquery" src={jquery}></img>jQuery |{" "}
+            <img className="skill-icon" src={mongoose}></img>Mongoose
           </TabPane>
           <TabPane name="Database" key="3">
-            MongoDB | PostgreSQL
+            <img className="skill-icon" src={mongodb}></img>MongoDB |{" "}
+            <img className="skill-icon" src={postgresql}></img>PostgreSQL
           </TabPane>
           <TabPane name="Tools" key="4">
-            Git | GitHub | RESTful APIs | MERN/PERN stack applications
+            <img className="skill-icon" src={git}></img>Git |{" "}
+            <img className="skill-icon" src={github}></img>GitHub |{" "}
+            <img className="skill-icon" src={api}></img>RESTful APIs
           </TabPane>
         </Tabs>
       </div>
