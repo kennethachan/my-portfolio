e61afb548fcacbb7ed80e7402ae161f4a2daada4
kennethachan <kennethandrechan@gmail.com>
2022-09-07 22:36:24 -0400

    updated about

--- src/pages/About.js
+++ src/pages/About.js
@@ -46,13 +46,14 @@ function About(props) {
       </div>
       <div className="about-me">
         <p>
-          I am a graduate of General Assembly's Software Engineering Immersive
-          Program with a non-coding background in architecture. My studies and
-          experience in the architectural field had fostered in me an interest
-          in social impact, sustainability, aesthetics, authenticity, design and
-          most importantly to find solutions to real world problems using
-          technology. I am currently applying the skills I gained to continue
-          building projects and search for career opportunities in tech.
+          Welcome to my portfolio! I am a graduate of General Assembly's
+          Software Engineering Immersive Program with a non-coding background in
+          architecture. My studies and experience in the architectural field had
+          fostered in me an interest in social impact, sustainability,
+          aesthetics, authenticity, design and most importantly to find
+          solutions to real world problems using technology. I am currently
+          applying the skills I gained to continue building projects and search
+          for career opportunities in tech.
           <br></br>
           <br></br>A little bit more about me, I enjoy skateboarding, DIY
           building, film photography and film!
