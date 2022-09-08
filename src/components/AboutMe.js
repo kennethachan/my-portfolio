d280eda329ec274294f35b46a01103c66ac41d60
kennethachan <kennethandrechan@gmail.com>
2022-09-08 11:34:47 -0400

    updated brand statement layout

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -8,14 +8,14 @@ function AboutMe(props) {
         <div className="about-details">
           <h1 className="name">Kenneth Chan</h1>
           <h3 className="job">Full Stack Developer</h3>
-          {/* <p className="brand">
+          <p className="brand">
             Full Stack Developer based in the NYC area with a diverse background
             ranging from woodworking to architectural design. I am a creator. I
             am always curious. I have a strong ambition to create work that is -
             <span className="bold-brand">
               uniquely enjoyable and fulfilling to the user’s experience.
             </span>
-          </p> */}
+          </p>
         </div>
       </div>
     </div>
