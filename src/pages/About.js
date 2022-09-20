12c19e5c2755c8aef8b96c9adca27a3fb1055d00
kennethachan <kennethandrechan@gmail.com>
2022-09-20 15:10:29 -0400

    updated about me

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,6 +1,8 @@
 import React from "react"
 import "./About.css"
 import profilePic from "../pictures/profile-pic3.jpg"
+import sk8 from "../pictures/sk8.png"
+import earlygrab from "../pictures/early-grab.jpg"
 
 function About(props) {
   return (
@@ -8,7 +10,7 @@ function About(props) {
       <div className="res-heading">
         <div>
           <img
-            className="profile-pic"
+            className="main-pic"
             src={profilePic}
             alt="profile picture of kenneth chan"
           ></img>
@@ -26,6 +28,18 @@ function About(props) {
           applying the skills I gained to continue building projects and search
           for career opportunities in tech.
           <br></br>
+          <div className="sk8-pic-container">
+            <img
+              className="sk8-pic"
+              src={sk8}
+              alt="profile picture of kenneth chan"
+            ></img>
+            <img
+              className="sk8-pic"
+              src={earlygrab}
+              alt="profile picture of kenneth chan"
+            ></img>
+          </div>
           <br></br>A little bit more about me, I enjoy skateboarding, DIY
           building, film photography and film!
         </p>
